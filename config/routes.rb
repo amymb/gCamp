7b3498cb22381345928685c1911f2c94bda3a066
Amy Bertken <amy.bertken@gmail.com>
2015-01-28 15:54:31 -0700

    add About and Terms pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/about' => 'about#index'
+  get '/terms' => 'terms#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
