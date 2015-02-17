8c90c87bd4338e44ed9bf1a279912eb597526c2f
Amy Bertken <amy.bertken@gmail.com>
2015-02-17 15:58:55 -0700

    add edit page and links, move user form to partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update
+      flash[:notice] = "User was successfully updated"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+
+
   private
   def user_params
       params.require(:user).permit(:first_name, :last_name, :email)
