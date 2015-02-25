50f8ec98240231318db19d32983694a8257af1a3
Amy Bertken <amy.bertken@gmail.com>
2015-02-25 16:36:16 -0700

    refactor user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,36 @@
 require 'rails_helper'
 
 describe User do
+  before :each do
+    @valid_attributes = {
+      first_name: "Amy",
+      last_name: "Bertken",
+      email: "amy@test.com"
+    }
+  end
+  
   it "validates user with first, last name, and email" do
-    user = User.create(first_name: "Amy", last_name: "Bertken", email: "amy@test.com")
+    user = User.create(@valid_attributes)
     expect(user).to be_valid
   end
 
   it "is invalid when email is not unique" do
-    User.create(first_name: "J", last_name: "B", email: "j@test.com")
-    user = User.create(first_name: "B", last_name: "C", email: "j@test.com")
+    User.create(@valid_attributes)
+    user = User.create(first_name: "B", last_name: "C", email: "amy@test.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
 
   it "is invalid without a first name" do
-    user = User.create(first_name: nil, last_name: "b", email: "b@test.com")
+    @valid_attributes.delete(:first_name)
+    user = User.create(@valid_attributes)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it "is invalid without a last name" do
-    user = User.create(first_name: "b", last_name: nil, email: "b@test.com")
+    @valid_attributes.delete(:last_name)
+    user = User.create(@valid_attributes)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
