66ea507d054fcee34443a9b20779568e64479eb5
Amy Bertken <amy.bertken@gmail.com>
2015-03-02 20:27:51 -0700

    error message for user sign in

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,25 @@
 class AuthenticationController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "You have successfully signed in"
+      redirect_to root_path
+    else
+      flash[:fun] = "Email / Password combination is invalid"
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "You have successfully logged out"
+    flash[:notice] = "You have successfully signed out"
     redirect_to root_path
   end
+
+
 end
