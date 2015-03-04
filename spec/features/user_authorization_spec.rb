ac6972140e4bd3321a1bf6ebec8260f8c1777490
Amy Bertken <amy.bertken@gmail.com>
2015-03-04 15:49:34 -0700

    add user authorization spec for sign up, sign in

---
+++ spec/features/user_authorization_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+feature "user authorization" do
+  scenario "user can sign up" do
+    visit root_path
+    click_link "Sign up"
+    click_button "Sign Up"
+    expect(page).to have_content "Password can't be blank"
+    expect(page).to have_content "Email can't be blank"
+
+    fill_in "First name", with: "Pete"
+    fill_in "Last name", with: "Repeat"
+    fill_in "Email", with: "pete@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "rabbit"
+    click_button "Sign Up"
+
+    expect(page).to have_content "Password confirmation doesn't match Password"
+
+
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign Up"
+
+    expect(page).to have_content "You have successfully signed up!"
+    expect(page).to have_content "Your life, organized"
+  end
+
+  scenario "user can sign in" do
+    visit root_path
+    funuser = User.new(first_name: "Little Bo", last_name: "Peep", email: "little@email.com", password: "12345", password_confirmation: "12345")
+    funuser.save!
+    click_link "Sign in"
+    click_button "Sign In"
+    expect(page).to have_content "Email / Password combination is invalid"
+    fill_in "Email", with: "little@email.com"
+    fill_in "Password", with: "12345"
+    click_button "Sign In"
+
+    expect(page).to have_content "You have successfully signed in"
+    expect(page).to have_content "Fine-grained permissions"
+  end
+
+end
