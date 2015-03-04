9938b765cc599338a01e7c0ed9cb06129d38a7f6
Amy Bertken <amy.bertken@gmail.com>
2015-03-04 16:04:10 -0700

    add user logout spec

--- spec/features/user_authorization_spec.rb
+++ spec/features/user_authorization_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature "user authorization" do
+
   scenario "user can sign up" do
     visit root_path
     click_link "Sign up"
@@ -38,7 +39,20 @@ feature "user authorization" do
     click_button "Sign In"
 
     expect(page).to have_content "You have successfully signed in"
-    expect(page).to have_content "Fine-grained permissions"
+    expect(page).to have_content "Little Bo Peep"
+
+    click_link "Little Bo Peep"
+    expect(page).to have_content "little@email.com"
+    expect(page).to have_content "Projects"
+    expect(page).to have_content "Tasks"
+  end
+
+  scenario "user can sign out" do
+    visit root_path
+    sign_in_user
+    click_link "Sign Out"
+    expect(page).to have_content "You have successfully signed out"
+    expect(page).to have_content "Your life, organized"
   end
 
 end
