0dfa3364006c078ea9effb12c05d6603fde28507
Amy Bertken <amy.bertken@gmail.com>
2015-03-06 17:21:04 -0700

    users cannot be added more than once to each project

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,5 @@ class Membership < ActiveRecord::Base
   ROLE = ['Member', 'Owner']
   validates_inclusion_of :role, :in => ROLE
   validates :user_id, presence: true
+  validates :user_id, uniqueness: {scope: :project_id, :message => "has already been added to this project"}
 end
